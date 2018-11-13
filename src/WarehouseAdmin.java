943e9a3ac02b2ed3a91a6015e8a323d38e86e2b6
Appy1412 <apoorv17027@iiitd.ac.in>
2018-11-13 19:06:58 +0530

    Changed WarehouseAdmin and added StoreAdmin

--- src/WarehouseAdmin.java
+++ src/WarehouseAdmin.java
@@ -1,2 +1,18 @@
-public class WarehouseAdmin {
+public class WarehouseAdmin extends User {
+
+    Warehouse myWarehouse;
+
+    WarehouseAdmin(String name, String id, String password, Warehouse warehouse) {
+        super(name, id, password);
+        this.myWarehouse = warehouse;
+    }
+
+    void addProduct(Product product) {
+        this.myWarehouse.addProduct(product);
+    }
+
+    void deleteProduct(Product product) {
+        this.myWarehouse.deleteProduct(Product);
+    }
+
 }
