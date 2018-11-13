ddaec9e55f35ac9625de4f6886377cd4f53a52b1
Gaurav Aggarwal <gaurav17288@iiitd.ac.in>
2018-11-13 14:15:03 +0530

    Added bunch of files

---
+++ src/sample/StoreDatabase.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class StoreDatabase {
+    List<Building> stores = new ArrayList<Building>();
+
+    public List<Building> getStores() {
+        return stores;
+    }
+
+    public void SetStore<>
+
+}
