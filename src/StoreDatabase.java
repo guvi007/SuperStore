267ef0984167a940ba892e376e9bcf52585ae123
Gaurav Aggarwal <gaurav17288@iiitd.ac.in>
2018-11-29 17:46:56 +0530

    Added StoreScreen for SuperUser.

--- src/StoreDatabase.java
+++ src/StoreDatabase.java
@@ -1,26 +1,46 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class StoreDatabase {
 
-    private List<Building> stores;
+    private HashMap<String, Building> stores;
+    private HashMap<String, User> storeAdmins;
+    private HashMap<String, String> nameToIdStores, nameToIdAdmins;
+    private int numStores;
 
     StoreDatabase() {
-        stores = new ArrayList<Building>();
+        stores = new HashMap<>();
+        nameToId = new HashMap<>();
+        numStores = 0;
     }
 
-    Store searchStore(String storeID) {
-        for (Store store : stores) {
-            if (store.getId.equals(storeID))
-                return store;
-        }
+    Store searchStore(String ) {
+
     }
 
-    List <Building> getStoresList() {
-        return this.stores;
+    List<Building> getStoresList() {
+
     }
 
-    void addStore(Store store) {
-        this.stores.add(store);
+    boolean checkId(String Id) {
+        if(storeAdmins.containsKey(Id))
+            return false;
+        return true;
+    }
+
+    void addStore(String storeName, Building Warehouse, String adminName, String adminLogin, String adminPassword) {
+        String storeId = "S" + numStores;
+
+        Building store = new Store(storeName,storeId , Warehouse);
+        User storeAdmin = new StoreAdmin(adminName, adminLogin, adminPassword, store);
+        store.setMyAdmin(storeAdmin);
+        ++numStores;
+
+        stores.put(storeId, store);
+        nameToIdStores.put(storeName, storeId);
+
+        storeAdmins.put(adminLogin, storeAdmin);
+        nameToIdAdmins.put(adminName, adminLogin);
     }
 }
